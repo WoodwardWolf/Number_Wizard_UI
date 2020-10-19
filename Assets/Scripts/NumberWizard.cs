c5aa1f0fed212c0550918281832910e9fde1640b
Gabriel Gilliam <36512950+WoodwardWolf@users.noreply.github.com>
2020-10-18 20:34:27 -0400

    Add files via upload

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+
+    [SerializeField] int maxGuess;
+    [SerializeField] int minGuess;
+    [SerializeField] TextMeshProUGUI guessText;
+
+    int guess;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        NextGuess();
+    }
+
+    public void TooHigh()
+    {
+        maxGuess = guess - 1;
+        NextGuess();
+    }
+
+    public void TooLow()
+    {
+        minGuess = guess + 1;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = Random.Range(minGuess, maxGuess + 1);
+        guessText.text = guess.ToString();
+    }
+}
+
